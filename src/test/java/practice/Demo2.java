bddbd47fea8f03e1b7e78520b667ba9077060776
Krithika <krithika.xworkz@gmail.com>
2026-03-11 13:07:08 +0530

    this is a commit message

--- src/test/java/practice/Demo2.java
+++ src/test/java/practice/Demo2.java
@@ -5,6 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 	
 		System.out.println("Practicing pull Operation");
+		System.out.println("Performing project request by creating new branch");
 		
 	}
 	
